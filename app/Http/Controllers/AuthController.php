2c84cb17a975b055293716ca9ff9b37955862bed
sosualfred <sosualfred@gmail.com>
2021-02-27 19:56:07 +0000

    fix(auth): use device id to create tokens
    Reviewed-by: @sosualfred
    Refs Use device name to create api tokens #7

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,6 +16,7 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $attr = $request->validate([
+            'deviceId' => 'required|string',
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|unique:users,email',
             'password' => 'required|string|min:8',
@@ -29,23 +30,29 @@ class AuthController extends Controller
 
         return $this->success([
             'user' => $user,
-            'token' => $user->createToken('API Token')->plainTextToken
+            'token' => $user->createToken($attr['deviceId'])->plainTextToken
         ]);
     }
 
     public function login(Request $request)
     {
         $attr = $request->validate([
+            'deviceId' => 'required|string',
             'email' => 'required|string|email|',
             'password' => 'required|string|min:8'
         ]);
 
-        if (!Auth::attempt($attr)) {
+        $userCredentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if (!Auth::attempt($userCredentials)) {
             return $this->error('Credentials not match', 401);
         }
 
         return $this->success([
-            'token' => auth()->user()->createToken('API Token')->plainTextToken
+            'token' => auth()->user()->createToken($attr['deviceId'])->plainTextToken
         ]);
     }
 
