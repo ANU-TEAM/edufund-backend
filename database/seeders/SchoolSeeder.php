c633eacdb94fcd1e3a8723eb4eb4de3ce84589d0
sosualfred <sosualfred@gmail.com>
2021-06-27 09:08:32 +0000

    add school to applications
    fixes #38

---
+++ database/seeders/SchoolSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use \App\Models\School;
+
+class SchoolSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        School::create([
+            'abbr' => 'ANU',
+            'name' => 'All Nations University'
+        ]);
+        School::create([
+            'abbr' => 'UG',
+            'name' => 'University of Ghana'
+        ]);
+        School::create([
+            'abbr' => 'KNUST',
+            'name' => 'Kwame Nkrumah University of Science and Technology'
+        ]);
+    }
+}
